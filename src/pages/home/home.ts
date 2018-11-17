59ba5b62eb306ebc3eb2f0a37cac0b81c85b83e9
Marcin <szemelaa@o2.pl>
2018-11-17 20:08:10 +0100

    added example with adding new events

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,5 +10,31 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  public currentEvents = [
+    {
+      year: 2018,
+      month: 9,
+      date: 25,
+      eventCSS:'red-one'
+    },
+
+  ];
+
+  public counter = 0;
+
+  addNewEvent()
+  {
+    let day = new Date();
+    day.setDate(day.getDate()+this.counter);
+    this.currentEvents.push({
+      year: day.getFullYear(),
+      month: day.getMonth(),
+      date: day.getUTCDate(),
+      eventCSS:'red-one'
+    });
+    this.counter++;
+    //line below trigger ngOnChanges in calendar
+    this.currentEvents.slice();
+  }
 
 }
